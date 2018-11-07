37c45909879382e72a18b85cbf41554526075baa
nico <nf@gutai-training.com>
2018-11-07 14:19:20 +0100

    Modification embedded.js

--- Scripts/embedded.js
+++ Scripts/embedded.js
@@ -6,14 +6,27 @@ Date : 05/11/2018
 */
 
 window.onload = function heightWindow() {
-setCaptors(40, 18, 6, 28, 145);
+setCaptors(14, 18, 6, 28, 148);
 };
 
 function setCaptors(wind, humidity, pression, temperature, altitude) {
-  if (wind != null)
-  document.getElementById("wind-captor").innerHTML = "Vent : " + wind + "Km/h";
-  document.getElementById("humidity-captor").innerHTML = "Humidité : " + humidity + "%";
-  document.getElementById("pression-captor").innerHTML = "Pression : " + pression + "bars";
-  document.getElementById("temperature-captor").innerHTML = "Température : " + temperature + "°C";
-  document.getElementById("altitude-captor").innerHTML = "Altitude : " + altitude + "m";
+  if (wind != null) {
+    console.log("test");
+    document.getElementById("wind-captor").innerHTML = "Vent : " + wind + "Km/h";
+  }
+  if (humidity != null) {
+    document.getElementById("humidity-captor").innerHTML = "Humidité : " + humidity + "%";
+  }
+  if (pression != null)
+  {
+    document.getElementById("pression-captor").innerHTML = "Pression : " + pression + "bars";
+  }
+  if (temperature != null)
+  {
+    document.getElementById("temperature-captor").innerHTML = "Température : " + temperature + "°C";
+  }
+  if (altitude != null)
+  {
+    document.getElementById("altitude-captor").innerHTML = "Altitude : " + altitude + "m";
+  }
 }
