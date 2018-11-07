762860362cb0d1ab5582244715d298a8d6581374
nico <nf@gutai-training.com>
2018-11-07 15:29:45 +0100

    Ajout dossier map-icons + ballon icon Ajout OpenStreetMap Correction erreurs mineures dans la console. Modification embedded.js -> Ajout fonction initMap + addMarker + Ajout de vérifications des variables

--- Scripts/embedded.js
+++ Scripts/embedded.js
@@ -5,13 +5,21 @@ Version : 0.1
 Date : 05/11/2018
 */
 
+var lat = 43.219839;
+var lon = 5.520354;
+
+var macarte = null;
+
+var iconFolder = "images/map-icons/"
+
 window.onload = function heightWindow() {
-setCaptors(14, 18, 6, 28, 148);
+  setCaptors(14, 18, 6, 28, 148);
+  initMap();
+  addMarker(lat, lon)
 };
 
 function setCaptors(wind, humidity, pression, temperature, altitude) {
   if (wind != null) {
-    console.log("test");
     document.getElementById("wind-captor").innerHTML = "Vent : " + wind + "Km/h";
   }
   if (humidity != null) {
@@ -29,4 +37,28 @@ function setCaptors(wind, humidity, pression, temperature, altitude) {
   {
     document.getElementById("altitude-captor").innerHTML = "Altitude : " + altitude + "m";
   }
+}
+
+// Fonction d'initialisation de la carte
+function initMap() {
+  // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+          macarte = L.map('map').setView([lat, lon], 11);
+          // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+          L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+              // Il est toujours bien de laisser le lien vers la source des données
+              attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+              minZoom: 1,
+              maxZoom: 20
+          }).addTo(macarte);
+}
+
+function addMarker(lat, lon) {
+  if (lat != null && lon != null && macarte != null)
+  var myIcon = L.icon({
+			iconUrl: iconFolder + "hot-air-balloon.png",
+			iconSize: [50, 50],
+			iconAnchor: [25, 50],
+			popupAnchor: [-3, -76],
+		});
+    var marker = L.marker([lat, lon], { icon : myIcon}).addTo(macarte);
 }
