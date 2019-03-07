953290bd3aa6e369908b4c50dfa413fda28fd7cf
nico <nf@gutai-training.com>
2019-03-07 18:28:33 +0100

    panel admin v2.js -> mise à la norme du code

--- Scripts/panel-admin-V2.js
+++ Scripts/panel-admin-V2.js
@@ -6,63 +6,15 @@ $(document).ready(function(){
 
     manageuser.classList.add('selected');
     bc.load('manageuser.html', function() {
-        var manageuserembedded = $("#allFacets, #userFacets");
-
-        $(function() {
-            manageuserembedded.sortable({
-                connectWith: "ul",
-                placeholder: "placeholder",
-                delay: 150
-            })
-                .disableSelection()
-                .dblclick( function(e){
-                    var item = e.target;
-                    if (e.currentTarget.id === 'allFacets') {
-                        //move from all to user
-                        $(item).fadeOut('fast', function() {
-                            $(item).appendTo($('#userFacets')).fadeIn('slow');
-                        });
-                    } else {
-                        //move from user to all
-                        $(item).fadeOut('fast', function() {
-                            $(item).appendTo($('#allFacets')).fadeIn('slow');
-                        });
-                    }
-                });
-        });
-
+        initEmbeddedSwitchManager();
     });
 
 
     $('#manageuser').click(function(){
       removeClass(divSelected, 'selected');
       manageuser.classList.add('selected');
-        bc.load('manageuser.html', function() {
-            var manageuserembedded = $("#allFacets, #userFacets");
-
-            $(function() {
-                manageuserembedded.sortable({
-                    connectWith: "ul",
-                    placeholder: "placeholder",
-                    delay: 150
-                })
-                    .disableSelection()
-                    .dblclick( function(e){
-                        var item = e.target;
-                        if (e.currentTarget.id === 'allFacets') {
-                            //move from all to user
-                            $(item).fadeOut('fast', function() {
-                                $(item).appendTo($('#userFacets')).fadeIn('slow');
-                            });
-                        } else {
-                            //move from user to all
-                            $(item).fadeOut('fast', function() {
-                                $(item).appendTo($('#allFacets')).fadeIn('slow');
-                            });
-                        }
-                    });
-            });
-
+        bc.load('manageuser.html', function () {
+            initEmbeddedSwitchManager();
         });
     });
 
@@ -75,6 +27,33 @@ $(document).ready(function(){
 
 
 });
+
+function initEmbeddedSwitchManager() {
+    var manageuserembedded = $("#allFacets, #userFacets");
+
+    $(function() {
+        manageuserembedded.sortable({
+            connectWith: "ul",
+            placeholder: "placeholder",
+            delay: 150
+        })
+            .disableSelection()
+            .dblclick( function(e){
+                var item = e.target;
+                if (e.currentTarget.id === 'allFacets') {
+                    //move from all to user
+                    $(item).fadeOut('fast', function() {
+                        $(item).appendTo($('#userFacets')).fadeIn('slow');
+                    });
+                } else {
+                    //move from user to all
+                    $(item).fadeOut('fast', function() {
+                        $(item).appendTo($('#allFacets')).fadeIn('slow');
+                    });
+                }
+            });
+    });
+}
 
 function removeClass(divSelected, removeElement) {
   for(var i=0; i < divSelected.length; i++){
