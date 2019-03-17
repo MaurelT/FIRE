f955c6f80b3e8e5b8a4881de2664f449a183bb9a
nico <nf@gutai-training.com>
2019-03-17 13:52:14 +0100

    Ajout de la détection de drag & drop fonctionnel Todo :     -> call API     -> Création dynamique de la liste de embedded     -> call API pour modifier les embedded

--- Scripts/panel-admin-V2.js
+++ Scripts/panel-admin-V2.js
@@ -35,7 +35,7 @@ function uniqueSwitchManager(switchable) {
       switchable.sortable({
           connectWith: "ul",
           placeholder: "placeholder",
-          delay: 150
+          delay: 150,
       })
           .disableSelection()
           .dblclick( function(e){
@@ -47,6 +47,7 @@ function uniqueSwitchManager(switchable) {
               }
           });
   });
+
 }
 
 function initEmbeddedSwitchManager() {
@@ -64,15 +65,35 @@ function initEmbeddedSwitchManager() {
                 if (e.currentTarget.id === 'allFacets') {
                     //move from all to user
                     $(item).fadeOut('fast', function() {
+                        console.log("Enlever embedded d'un utilisateur DB click");
                         $(item).appendTo($('#userFacets')).fadeIn('slow');
                     });
-                } else {
+                } else if (e.currentTarget.id === 'userFacets' ) {
                     //move from user to all
                     $(item).fadeOut('fast', function() {
+                        console.log("assigner embedded à un utilisateur DB click");
                         $(item).appendTo($('#allFacets')).fadeIn('slow');
                     });
                 }
-            });
+            })
+            .droppable({
+            drop: function(e) {
+
+                var target = e.target;
+                //var item = $(this).data().uiSortable.currentItem[0].id;
+
+
+                //var test = $(this).data().uiSortable.currentItem;
+                //console.log(test);
+
+                if (target.id === 'allFacets') {
+                    console.log("Assigner à utilisateur drop");
+                }
+                else if (target.id === 'userFacets') {
+                    console.log("Désassigner d'un utilisateur drop");
+                }
+            }
+        });
     });
 }
 
