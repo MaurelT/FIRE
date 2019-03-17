6192e58acd0fdaddfc5f28ced57064ab3d910d7e
MaurelT <relmaurel@gmail.com>
2019-03-17 20:01:52 +0000

    Create User Page

--- Scripts/panel-admin-V2.js
+++ Scripts/panel-admin-V2.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+
+    //CONTROLER DESKTOP
     var bc = $('#bc');
     var divSelected = document.getElementsByClassName('itemPosition');
     var manageuser = document.getElementById('manageuser');
     var manageembedded = document.getElementById('manageembedded');
+    var createuser = document.getElementById('createuser');
 
     manageuser.classList.add('selected');
     bc.load('manageuser.html', function() {
@@ -10,6 +13,12 @@ $(document).ready(function(){
         uniqueSwitchManager($('#usersList'));
     });
 
+    $('#createuser').click(function () {
+      removeClass(divSelected, 'selected');
+      bc.load('createuser.html', function() {
+        initcreateuser();
+      });
+    });
 
     $('#manageuser').click(function(){
       removeClass(divSelected, 'selected');
@@ -22,14 +31,42 @@ $(document).ready(function(){
     $('#manageembedded').click(function(){
       removeClass(divSelected, 'selected');
       manageembedded.classList.add('selected');
-      bc.load('manageembedded.html');
+      bc.load('manageembedded.html' , function () {
+          initEmbeddedSwitchManager();
+        });
+    });
+
+    //CONTROLER MOBILE
+    var mdivSelected = document.getElementsByClassName('m-itemPosition');
+    var mmanageuser = document.getElementById('m-manageuser');
+    var mmanageembedded = document.getElementById('m-manageembedded');
+
+    mmanageuser.classList.add('selected');
+    bc.load('manageuser.html', function() {
         initEmbeddedSwitchManager();
+    });
+
+    $('#m-manageuser').click(function(){
+      removeClass(mdivSelected, 'selected');
+      mmanageuser.classList.add('selected');
+        bc.load('manageuser.html', function () {
+            initEmbeddedSwitchManager();
+        });
+    });
+
+    $('#m-manageembedded').click(function(){
+      removeClass(mdivSelected, 'selected');
+      mmanageembedded.classList.add('selected');
+      bc.load('manageembedded.html' , function () {
+          initEmbeddedSwitchManager();
+        });
     });
 
 
 
 });
 
+//FUCNTION FOR CONTROLER M & D
 function uniqueSwitchManager(switchable) {
   $(function() {
       switchable.sortable({
@@ -88,4 +125,144 @@ function removeClass(divSelected, removeElement) {
       child.classList.remove("selected");
     }
   }
+}
+
+//REQUEST INFORMATION
+var userTableListPage = 0;
+
+function getCookieVal(offset) {
+    var endstr=document.cookie.indexOf (";", offset);
+    if (endstr==-1) endstr=document.cookie.length;
+    return unescape(document.cookie.substring(offset, endstr));
+}
+function GetCookie (name) {
+    var arg=name+"=";
+    var alen=arg.length;
+    var clen=document.cookie.length;
+    var i=0;
+    while (i<clen) {
+        var j=i+alen;
+        if (document.cookie.substring(i, j)==arg) return getCookieVal (j);
+        i=document.cookie.indexOf(" ",i)+1;
+        if (i==0) break;
+    }
+    return null;
+}
+
+/* INIT CREATE USER FUNCTIONS */
+function initcreateuser() {
+    $("#submit").click(function(){
+
+        if (noEmptyInput() && checkPassword()) {
+            var name = $('#name').val();
+            var first_name = $('#firstName').val();
+            var user_name = $('#userName').val();
+            var password = $('#password').val(); //ENCRYPTER LE PASSWORD
+            var rank = $('#roleSelect').val();
+            var email = $('#emailAddress').val();
+
+
+            let userTmp = JSON.parse(GetCookie("UserTmp"));
+            let token = userTmp['token'];
+            let newUser = { name:name, first_name:first_name, user_name:user_name, password:password, rank:rank,email:email };
+
+            console.log("initTabOne");
+
+            $.post({
+                type: "POST",
+                url: "http://109.255.19.77:81/API/User/create.php",
+                headers: {'Authorization': token},
+                data: JSON.stringify(newUser),
+                dataType:"JSON",
+                success: function(response) {
+                    alert("user created");
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    alert("some error");
+                }
+            })
+        }
+    })
+}
+
+
+//FUNCTIONS FOR initcreateuser
+function checkPassword() {
+
+    var password = $('#password').val();
+    var confirmPassword = $('#confirmPassword').val();
+
+    var upperCase= new RegExp('[A-Z]');
+    var lowerCase= new RegExp('[a-z]');
+    var numbers = new RegExp('[0-9]');
+
+
+    if(password.match(upperCase) && password.match(lowerCase) &&   password.match(numbers)) {
+        $("#password").removeClass("is-invalid");
+        $("#passwordHelp").removeClass("text-danger");
+        $("#passwordHelp").addClass("text-muted");
+    }
+    else {
+        $("#password").addClass("is-invalid");
+        $("#passwordHelp").addClass("text-danger");
+        $("#passwordHelp").removeClass("text-muted");
+    }
+
+    if (password == confirmPassword && password.length > 7 && confirmPassword.length > 7) {
+        $("#confirmPassword").removeClass("is-invalid");
+        $("#confirmPasswordHelp").removeClass("text-danger");
+        $("#confirmPasswordHelp").addClass("text-muted");
+
+        return (true);
+    }
+    $("#confirmPassword").addClass("is-invalid");
+    $("#confirmPasswordHelp").addClass("text-danger");
+    $("#confirmPasswordHelp").removeClass("text-muted");
+    return (false);
+}
+
+function  noEmptyInput() {
+    var name = $('#name').val()
+    var firstName = $('#firstName').val()
+    var userName = $('#userName').val()
+    var emailAddress = $('#emailAddress').val()
+    var bool = true;
+
+    if (!name.trim()) {
+        $("#name").addClass("is-invalid");
+        bool = false
+    }
+    else {
+        $("#name").removeClass("is-invalid");
+    }
+
+
+    if (!firstName.trim()) {
+        $("#firstName").addClass("is-invalid");
+        bool = false;
+    }
+    else {
+        $("#firstName").removeClass("is-invalid");
+    }
+
+
+    if (!userName.trim()) {
+        $("#userName").addClass("is-invalid");
+        bool = false;
+    }
+    else {
+        $("#userName").removeClass("is-invalid");
+    }
+
+
+    if (!emailAddress.trim()) {
+        $("#emailAddress").addClass("is-invalid");
+        bool = false;
+    }
+    else {
+        $("#emailAddress").removeClass("is-invalid");
+    }
+
+
+    return (bool);
 }
