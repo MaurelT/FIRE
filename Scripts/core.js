0b3b1b9fd8cad334a4d9fe8373016ce06a8c1a68
MaurelT <relmaurel@gmail.com>
2019-05-20 15:55:37 +0100

    Service Worker

--- Scripts/core.js
+++ Scripts/core.js
@@ -69,10 +69,9 @@ $(document).ready(function() {
     });
 });
 
-/*
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('../service-worker.js')
         .then(function() { console.log('Service Worker Registered');
         })
-} */
+}
